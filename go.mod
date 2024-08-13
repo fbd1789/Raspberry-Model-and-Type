ea91b1fcaa7220132490c22de81b4d0e21ab7ae4
fbd1789 <fabien@familledelmotte.eu>
2024-08-13 16:42:10 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ssh_connection
+
+go 1.21.6
+
+require (
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+    "strings"
+	"os"
+	"golang.org/x/crypto/ssh"
+	"gopkg.in/ini.v1"
+)
+
+
+// SystemInfo represente les informations generales du système.
+type SystemInfo struct {
+	Revision  string
+	Serial    string
+	Model     string
+	Mem		  string
+}
+
+func main() {
+	inidata, err := ini.Load("config.ini")
+	if err != nil {
+	   fmt.Printf("Fail to read file: %v", err)
+	   os.Exit(1)
+	 }
+	UserName := inidata.Section("global").Key("username").String()
+	Password := inidata.Section("global").Key("password").String()
+	Host := inidata.Section("global").Key("device").String()
+
+
+	// Configuration SSH
+	config := &ssh.ClientConfig{
+		User: UserName,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // Pour ignorer la verification de l'empreinte du serveur
+	}
+
+	// Connexion SSH
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:22",Host), config)
+	if err != nil {
+		log.Fatalf("Failed to dial: %s", err)
+	}
+	defer client.Close()
+
+	// Creation de la session
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create session: %s", err)
+	}
+	defer session.Close()
+
+	// Recuperation de la sortie de la commande
+	var stdoutBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+
+	// Execution des commandes pour le modele et la memoire
+	cmd := "cat /proc/cpuinfo; free"
+	if err := session.Run(cmd); err != nil {
+		log.Fatalf("Failed to run: %s", err)
+	}
+
+	// Traitement du resultat
+	// Conversion de l'hexa en string
+	output := stdoutBuf.String()
+	// Delimitation des retours de ligne
+	lines := strings.Split(output, "\n")
+
+	// Definition pour la structure
+	var systemInfo SystemInfo
+	// Affichage des lignes delimitees
+	for _, line := range lines {
+		// Enlever les espaces en debut et fin de chaine
+		line = strings.TrimSpace(line)
+		// Separation de la cle et de la valeur
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		// Stockage cle:valeur
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		// Remplir les informations du système et de la memoire en fonction de la cle
+		switch key {
+		case "Revision":
+			systemInfo.Revision = value
+		case "Serial":
+			systemInfo.Serial = value
+		case "Model":
+			systemInfo.Model = value
+		case "Mem":
+			systemInfo.Mem = strings.Split(value, " ")[0]  //Il faut prendre uniquement la premiere valeur
+		}
+	}
+	fmt.Printf("Revision: %s\n",systemInfo.Revision)
+	fmt.Printf("Serial: %s\n",systemInfo.Serial)
+	fmt.Printf("Model: %s\n",systemInfo.Model)
+	fmt.Printf("Mem: %s\n",systemInfo.Mem)
+}
